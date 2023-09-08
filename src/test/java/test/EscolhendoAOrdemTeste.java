8b7fd954461901883cfd179113fabc808ca6a83d
do5-5anto5 <dos.santos.2k23@gmail.com>
2023-09-08 10:28:59 -0300

    MethodOrderer.MethodName - outra forma de ordenar testes

--- src/test/java/test/EscolhendoAOrdemTeste.java
+++ src/test/java/test/EscolhendoAOrdemTeste.java
@@ -3,35 +3,31 @@ package test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class EscolhendoAOrdemTeste {
 
 	
 	
-	@Order(4)
 	@Test
 	void validaFluxoA() {
 		assertTrue(true);
 	}
 	
-	@Order(3)
 	@Test
 	void validaFluxoB() {
 		assertTrue(true);
 	}
 	
-	@Order(2)
 	@Test
 	void validaFluxoC() {
 		assertTrue(true);
 	}
 	
-	@Order(1)
 	@Test
 	void validaFluxoD() {
 		assertTrue(true);
